75df7dadbf39a89eb40c431237aab2e4eba0dbc8
Alexis Barba <alexbarba.2278@gmail.com>
2022-12-16 20:47:29 -0600

    fix footer width

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className='footer position-absolute bottom-0 start-0 mw-100 d-block '>
-            <div class="container py-3 bg-dark">
-                <span className='text-light'>
-                    <p>Rafael de Jesús Anguiano Suárez del Real</p>
+        <div className='footer position-fixed w-100 bottom-0 start-0 mw-100 d-block p-3 bg-dark'>
+            <div class=" ">
+                <span className='text-light text-center'>
+                    <p className='m-0'>Rafael de Jesús Anguiano Suárez del Real</p>
                 </span>
             </div>
         </div>
